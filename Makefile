c031ad556aa5ef3a7186528bef7d98bbcc999e60
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-01-27 16:46:27 +0100

    Add first doctest.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: all install install-shell build-rust build-rust-release check-rust clean
+.PHONY: all install install-shell build-rust build-rust-release check-rust test-rust clean
 
 VERSION = 1.0.0
 PN = memoize
@@ -9,8 +9,10 @@ BINDIR = $(PREFIX)/bin
 SHSRC = src/sh
 
 RUSTPACKAGE = src/rust/memoize
+RUSTLIBPACKAGE = src/rust/memoize_lib
 RUSTSRC = $(RUSTPACKAGE)/src/bin
 RUSTBIN = _build/rust/bin
+RUSTLIB = _build/rust/lib
 RUSTSOURCES = $(shell find $(RUSTSRC) -type f)
 RUSTBINARIES = $(basename $(subst $(RUSTSRC),$(RUSTBIN),$(RUSTSOURCES)))
 
@@ -40,6 +42,12 @@ check-rust: $(RUSTSOURCES)
 		--verbose \
 		--manifest-path $(RUSTPACKAGE)/Cargo.toml \
 		--target-dir $(RUSTBIN)
+
+test-rust: $(RUST)
+	cargo test \
+		--verbose \
+		--manifest-path $(RUSTLIBPACKAGE)/Cargo.toml \
+		--target-dir $(RUSTLIB)
 
 clean:
 	rm -rf _build
--- src/rust/memoize_lib/src/args.rs
+++ src/rust/memoize_lib/src/args.rs
@@ -1,9 +1,15 @@
 use std::env;
 use std::process;
 
-pub fn parse ( ) -> (i64, Vec<String>) {
-
-    let args: Vec<String> = env::args().collect();
+/// ```
+/// use memoize_lib::args::_parse;
+/// let v1: Vec<&str> = "/path/bin 0 Mary had a little lamb".split(' ').collect();
+/// let v2: Vec<String> = v1.iter().map(|&s| String::from(s)).collect();
+/// let (validity, cmdline) = _parse(&v2);
+/// assert_eq!(validity, 0);
+/// assert_eq!(cmdline.join(" "), "Mary had a little lamb");
+/// ```
+pub fn _parse ( args: &Vec<String> ) -> (i64, Vec<String>) {
 
     let validity = &args[1];
     let validity: i64 = match validity.trim().parse() {
@@ -17,4 +23,9 @@ pub fn parse ( ) -> (i64, Vec<String>) {
     let cmdline = args[2..].to_vec();
 
     return (validity, cmdline);
+}
+
+pub fn parse ( ) -> (i64, Vec<String>) {
+    let args: Vec<String> = env::args().collect();
+    return _parse(&args);
 }
